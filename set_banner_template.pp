eab48525095c4f53e1f3b242b55e9eb6f7d063b9
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2025-07-30 17:59:33 -0400

    Update set_banner_template.pp

--- set_banner_template.pp
+++ set_banner_template.pp
@@ -1,13 +1,10 @@
-class cisco_banner {
-
-  $devices = ['10.10.10.1', '10.10.10.2', '10.10.10.3', '10.10.10.4', '10.10.10.5', '10.10.10.6', '10.10.10.7']
-
-  $devices.each |String $device| {
-    exec { "set_banner_${device}":
-      command => "sshpass -p '${CISCO_PASS}' ssh -o StrictHostKeyChecking=no ${CISCO_USER}@${device} 'enable ; ${ENABLE_PASS} ; conf t ; banner motd ^Welcome to ${device}^ ; write memory'",
-      path    => ['/usr/bin', '/bin'],
-    }
-  }
+exec { 'set_banner_10.10.10.1':
+  command => "sshpass -p '${CISCO_PASS}' ssh -o StrictHostKeyChecking=no ${CISCO_USER}@10.10.10.1 'enable ; ${ENABLE_PASS} ; conf t ; banner motd ^Welcome to 10.10.10.1^ ; end ; write memory'",
+  path    => ['/usr/bin', '/bin'],
 }
 
-include cisco_banner
+exec { 'set_banner_10.10.10.2':
+  command => "sshpass -p '${CISCO_PASS}' ssh -o StrictHostKeyChecking=no ${CISCO_USER}@10.10.10.2 'enable ; ${ENABLE_PASS} ; conf t ; banner motd ^Welcome to 10.10.10.2^ ; end ; write memory'",
+  path    => ['/usr/bin', '/bin'],
+}
+
