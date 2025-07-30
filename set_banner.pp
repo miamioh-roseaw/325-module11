d4388f55016ef1f27bc9f69e1a4c6af2d2ab2e34
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2025-07-30 17:35:05 -0400

    Update set_banner.pp

--- set_banner.pp
+++ set_banner.pp
@@ -7,13 +7,12 @@ class cisco_banner (
 
   $devices.each |$ip| {
     exec { "set_banner_${ip}":
-      command => "sshpass -p '${cisco_pass}' ssh -o StrictHostKeyChecking=no ${cisco_user}@${ip} 'conf t ; banner motd ^Welcome to ${ip}^ ; end ; write memory'",
+      command => "sshpass -p '${cisco_pass}' ssh -o KexAlgorithms=+diffie-hellman-group14-sha1,diffie-hellman-group1-sha1 -o StrictHostKeyChecking=no ${cisco_user}@${ip} 'conf t ; banner motd ^Welcome to ${ip}^ ; end ; write memory'",
       path    => ['/usr/bin', '/bin'],
     }
   }
 }
 
-# Apply the class with passed-in values
 class { 'cisco_banner':
   cisco_user => $::cisco_user,
   cisco_pass => $::cisco_pass,
