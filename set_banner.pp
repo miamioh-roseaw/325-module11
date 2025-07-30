b74fee5dcf0f31f9a4989c411037a4f929587750
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2025-07-30 17:46:45 -0400

    Update set_banner.pp

--- set_banner.pp
+++ set_banner.pp
@@ -4,20 +4,12 @@ class cisco_banner (
   String $enable_pass,
 ) {
 
-  $devices = [
-    '10.10.10.1',
-    '10.10.10.2',
-    '10.10.10.3',
-    '10.10.10.4',
-    '10.10.10.5',
-    '10.10.10.6',
-    '10.10.10.7',
-  ]
+  $devices = ['10.10.10.1', '10.10.10.2', '10.10.10.3', '10.10.10.4']
 
-  $devices.each |$ip| {
-    exec { "set_banner_${ip}":
-      command => "sshpass -p '${cisco_pass}' ssh -o StrictHostKeyChecking=no ${cisco_user}@${ip} 'conf t ; enable ; banner motd ^Welcome to ${ip}!^ ; end ; write memory'",
-      path    => ['/bin', '/usr/bin'],
+  $devices.each |String $device| {
+    exec { "set_banner_${device}":
+      command => "sshpass -p ${cisco_pass} ssh -o StrictHostKeyChecking=no ${cisco_user}@${device} 'conf t ; banner motd ^Welcome to ${device}^ ; enable ; write memory'",
+      path    => ['/usr/bin', '/bin'],
     }
   }
 }
