0962dd18b7c5dc8f322662973153b41fa83fdc59
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2025-07-31 08:50:21 -0400

    Update set_banner_template.pp

--- set_banner_template.pp
+++ set_banner_template.pp
@@ -1,10 +1,10 @@
 exec { 'set_banner_10.10.10.1':
-  command => "sshpass -p '${CISCO_PASS}' ssh -o StrictHostKeyChecking=no ${CISCO_USER}@10.10.10.1 'enable ; ${ENABLE_PASS} ; conf t ; banner motd ^Welcome to 10.10.10.1^ ; end ; write memory'",
+  command => "sshpass -p '${CISCO_PASS}' ssh -o StrictHostKeyChecking=no ${CISCO_USER}@10.10.10.1 'enable ; ${ENABLE_PASS} ; conf t ; banner motd ^Welcome to 10.10.10.1, configured by puppet.^ ; end ; write memory'",
   path    => ['/usr/bin', '/bin'],
 }
 
 exec { 'set_banner_10.10.10.2':
-  command => "sshpass -p '${CISCO_PASS}' ssh -o StrictHostKeyChecking=no ${CISCO_USER}@10.10.10.2 'enable ; ${ENABLE_PASS} ; conf t ; banner motd ^Welcome to 10.10.10.2^ ; end ; write memory'",
+  command => "sshpass -p '${CISCO_PASS}' ssh -o StrictHostKeyChecking=no ${CISCO_USER}@10.10.10.2 'enable ; ${ENABLE_PASS} ; conf t ; banner motd ^Welcome to 10.10.10.2, configured by puppet.^ ; end ; write memory'",
   path    => ['/usr/bin', '/bin'],
 }
 
