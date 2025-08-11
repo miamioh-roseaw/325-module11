5b9c9f66e30ec8bd10e5f81f72efc102c43a0ed7
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2025-08-11 15:15:16 -0400

    Update ntp_add.pp

--- ntp_add.pp
+++ ntp_add.pp
@@ -1,5 +1,3 @@
-# ntp_add.pp — minimal, robust (newline-fed), removes banners + sets NTP
-
 exec { 'cfg_10.10.10.1':
   command => 'printf "enable\n$ENABLE_PASS\nconf t\nno banner login\nno banner motd\nno banner exec\nno banner incoming\nno ntp server 129.6.15.28\nntp server 129.6.15.28\nno ntp server 129.6.15.29\nntp server 129.6.15.29\nend\nwrite memory\n" | sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.1"',
   path    => ['/usr/bin','/bin'],
