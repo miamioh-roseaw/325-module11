34407624dc732bcdb4a7ebe4770ec81745621b85
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2025-08-11 15:09:55 -0400

    Update ntp_add.pp

--- ntp_add.pp
+++ ntp_add.pp
@@ -1,37 +1,36 @@
-# Expects CISCO_USER / CISCO_PASS / ENABLE_PASS in env (Jenkins).
-# Also allows legacy SSH algos for older IOS.
+# ntp_add.pp — minimal, robust (newline-fed), removes banners + sets NTP
 
-exec { 'ntp_10.10.10.1':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.1" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
+exec { 'cfg_10.10.10.1':
+  command => 'printf "enable\n$ENABLE_PASS\nconf t\nno banner login\nno banner motd\nno banner exec\nno banner incoming\nno ntp server 129.6.15.28\nntp server 129.6.15.28\nno ntp server 129.6.15.29\nntp server 129.6.15.29\nend\nwrite memory\n" | sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.1"',
   path    => ['/usr/bin','/bin'],
 }
 
-exec { 'ntp_10.10.10.2':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.2" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
+exec { 'cfg_10.10.10.2':
+  command => 'printf "enable\n$ENABLE_PASS\nconf t\nno banner login\nno banner motd\nno banner exec\nno banner incoming\nno ntp server 129.6.15.28\nntp server 129.6.15.28\nno ntp server 129.6.15.29\nntp server 129.6.15.29\nend\nwrite memory\n" | sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.2"',
   path    => ['/usr/bin','/bin'],
 }
 
-exec { 'ntp_10.10.10.3':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.3" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
+exec { 'cfg_10.10.10.3':
+  command => 'printf "enable\n$ENABLE_PASS\nconf t\nno banner login\nno banner motd\nno banner exec\nno banner incoming\nno ntp server 129.6.15.28\nntp server 129.6.15.28\nno ntp server 129.6.15.29\nntp server 129.6.15.29\nend\nwrite memory\n" | sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.3"',
   path    => ['/usr/bin','/bin'],
 }
 
-exec { 'ntp_10.10.10.4':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.4" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
+exec { 'cfg_10.10.10.4':
+  command => 'printf "enable\n$ENABLE_PASS\nconf t\nno banner login\nno banner motd\nno banner exec\nno banner incoming\nno ntp server 129.6.15.28\nntp server 129.6.15.28\nno ntp server 129.6.15.29\nntp server 129.6.15.29\nend\nwrite memory\n" | sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.4"',
   path    => ['/usr/bin','/bin'],
 }
 
-exec { 'ntp_10.10.10.5':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.5" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
+exec { 'cfg_10.10.10.5':
+  command => 'printf "enable\n$ENABLE_PASS\nconf t\nno banner login\nno banner motd\nno banner exec\nno banner incoming\nno ntp server 129.6.15.28\nntp server 129.6.15.28\nno ntp server 129.6.15.29\nntp server 129.6.15.29\nend\nwrite memory\n" | sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.5"',
   path    => ['/usr/bin','/bin'],
 }
 
-exec { 'ntp_10.10.10.6':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.6" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
+exec { 'cfg_10.10.10.6':
+  command => 'printf "enable\n$ENABLE_PASS\nconf t\nno banner login\nno banner motd\nno banner exec\nno banner incoming\nno ntp server 129.6.15.28\nntp server 129.6.15.28\nno ntp server 129.6.15.29\nntp server 129.6.15.29\nend\nwrite memory\n" | sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.6"',
   path    => ['/usr/bin','/bin'],
 }
 
-exec { 'ntp_10.10.10.7':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.7" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
+exec { 'cfg_10.10.10.7':
+  command => 'printf "enable\n$ENABLE_PASS\nconf t\nno banner login\nno banner motd\nno banner exec\nno banner incoming\nno ntp server 129.6.15.28\nntp server 129.6.15.28\nno ntp server 129.6.15.29\nntp server 129.6.15.29\nend\nwrite memory\n" | sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.7"',
   path    => ['/usr/bin','/bin'],
 }
