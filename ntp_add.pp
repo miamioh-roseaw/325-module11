3fc72c4331d0498876c547e72873b7abb3212f28
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2025-08-11 15:00:51 -0400

    Update ntp_add.pp

--- ntp_add.pp
+++ ntp_add.pp
@@ -1,36 +1,37 @@
-# Minimal NTP push to all devices; expects CISCO_USER/CISCO_PASS/ENABLE_PASS in env
+# Expects CISCO_USER / CISCO_PASS / ENABLE_PASS in env (Jenkins).
+# Also allows legacy SSH algos for older IOS.
 
 exec { 'ntp_10.10.10.1':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.1" "enable ; $ENABLE_PASS ; conf t ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; no banner login ; no banner motd ; no banner exec ; end ; write memory"',
+  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.1" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
   path    => ['/usr/bin','/bin'],
 }
 
 exec { 'ntp_10.10.10.2':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.2" "enable ; $ENABLE_PASS ; conf t ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; no banner login ; no banner motd ; no banner exec ; end ; write memory"',
+  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.2" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
   path    => ['/usr/bin','/bin'],
 }
 
 exec { 'ntp_10.10.10.3':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.3" "enable ; $ENABLE_PASS ; conf t ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
+  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.3" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
   path    => ['/usr/bin','/bin'],
 }
 
 exec { 'ntp_10.10.10.4':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.4" "enable ; $ENABLE_PASS ; conf t ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
+  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.4" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
   path    => ['/usr/bin','/bin'],
 }
 
 exec { 'ntp_10.10.10.5':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.5" "enable ; $ENABLE_PASS ; conf t ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
+  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.5" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
   path    => ['/usr/bin','/bin'],
 }
 
 exec { 'ntp_10.10.10.6':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.6" "enable ; $ENABLE_PASS ; conf t ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
+  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.6" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
   path    => ['/usr/bin','/bin'],
 }
 
 exec { 'ntp_10.10.10.7':
-  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.7" "enable ; $ENABLE_PASS ; conf t ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
+  command => 'sshpass -p "$CISCO_PASS" ssh -tt -o StrictHostKeyChecking=no -o KexAlgorithms=+diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa "$CISCO_USER@10.10.10.7" "enable ; $ENABLE_PASS ; conf t ; no banner login ; no banner motd ; no banner exec ; no banner incoming ; no ntp server 129.6.15.28 ; ntp server 129.6.15.28 ; no ntp server 129.6.15.29 ; ntp server 129.6.15.29 ; end ; write memory"',
   path    => ['/usr/bin','/bin'],
 }
